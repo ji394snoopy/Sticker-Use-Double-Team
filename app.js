6101c7ebf0217479adc5bf27e2ec9614e5420777
ji394snoopy <eric60513@gmail.com>
2018-11-12 14:51:26 +0800

    del config

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = require('./src/config');
 const webhook = require('./src/controller/webhook');
 const server = require('./src/server');
 // set bot key
-const bot = new Telebot(config.TELEBOT_KEY);
+const bot = new Telebot(process.env.TELEBOT_KEY);
 // set webhook
 webhook.set(bot);
 // start bot
